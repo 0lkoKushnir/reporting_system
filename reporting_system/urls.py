db4ac61bc967759be6c591026536ae7a81b80c92
Oleh Kushnir <olko.kushnir@gmail.com>
2021-05-29 17:39:47 +0300

    Implemented registration, login, profile

--- reporting_system/urls.py
+++ reporting_system/urls.py
@@ -14,11 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from user import views as user_views
+#from . import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='reporting_system-home'),
+    path('', user_views.home, name='reporting_system-home'),
+    #path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='reporting_system-home'),
     path('table/', include('table.urls')),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- reporting_system/views.py
+++ reporting_system/views.py
@@ -1,5 +1,14 @@
+
+from django.shortcuts import render
+from .settings import *
+import pyodbc
+
+
+def home(request):
+	return render(request, 'rs/home.html')
+'''
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	return HttpResponse('<h1>HOME</h1>')
+	return HttpResponse('<h1>HOME</h1>')'''
